31dbafb1c4ef00c5567f3ecb3831030d91145abb
Cristiana Vasconcelos <cristiana.aurea123@gmail.com>
2014-07-17 17:38:38 -0400

    [ADJUST] melhorando o CRUD
    Signed-off-by: Cristiana Vasconcelos <cristiana.aurea123@gmail.com>

--- editarC.php
+++ editarC.php
@@ -5,7 +5,7 @@ $id = $_POST['id'];
 $dia = $_POST['dia'];
 $paciente = $_POST['paciente'];
 $medico = $_POST['medico'];
-pg_query("UPDATE consultas   SET id=$id, dia=$dia, paciente='$paciente', medico_id= $medico WHERE id = $id;");
+pg_query("UPDATE consultas SET id=$id, dia='$dia', paciente='$paciente', medico_id= '$medico' WHERE id = $id;");
 header("location: consultas.php");
  
 
