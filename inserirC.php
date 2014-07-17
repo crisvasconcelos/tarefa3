d2f63d43392a63749b5521ec00b5d8ab48451233
Cristiana Vasconcelos <cristiana.aurea123@gmail.com>
2014-07-17 12:17:00 -0400

    [ADJUST] Adição de médico na inserção de consulta
    Signed-off-by: Cristiana Vasconcelos <cristiana.aurea123@gmail.com>

--- inserirC.php
+++ inserirC.php
@@ -3,8 +3,9 @@ include_once 'conexao.php';
 
 $dia= $_POST['dia'];
 $paciente = $_POST['paciente'];
+$medico = $_POST['medico'];
   
 if ($dia){
-pg_query("INSERT INTO consultas (dia, paciente) VALUES ('$dia','$paciente');");
+pg_query("INSERT INTO consultas (dia, paciente,medico_id) VALUES ('$dia','$paciente',$medico);");
 }
 header("location: consultas.php");
